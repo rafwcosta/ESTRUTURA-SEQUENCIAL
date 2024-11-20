6928f0fa24023d2aca70099c18700e5e9b6b5007
Rafael Costa <zrafaelcostaz@gmail.com>
2024-11-20 16:31:07 -0300

    Commit exercício 4

---
+++ src/Exercicio4.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        // Variáveis
+        int numeroDoFuncionario, horasTrabalhadas;
+        double valorQueRecebePorHora, salarioDoFuncionario;
+
+        // Entrada dos dados
+        System.out.print("Número do funcionário: ");
+        numeroDoFuncionario = sc.nextInt();
+        System.out.print("Horas trabalhadas: ");
+        horasTrabalhadas = sc.nextInt();
+        System.out.print("Valor que recebe por hora: ");
+        valorQueRecebePorHora = sc.nextDouble();
+
+        // Processamento dos dados
+        salarioDoFuncionario = horasTrabalhadas * valorQueRecebePorHora;
+
+        // Saída dos dados
+        System.out.println();
+        System.out.println("Número do funionário: " + numeroDoFuncionario);
+        System.out.printf("Salário: R$%.2f%n", salarioDoFuncionario);
+
+        // Fechamento do código
+        sc.close();
+
+    }
+}
